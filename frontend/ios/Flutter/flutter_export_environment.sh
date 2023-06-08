06ec70ff5eea474b4b3c7d89e454f1559256a0bb
Sejal Jaiswal <710sejal@gmail.com>
2023-06-08 16:10:38 +0530

    flutter build apk

--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\sejal\OneDrive\Desktop\flutter_project\my_first_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sejal\OneDrive\Desktop\kapiva\frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
